a371a36cf8e8b17a4433e94ed12dfc5c98da3109
Sentienta <sentientasins@gmail.com>
2021-10-21 13:58:00 +0530

    added all fields in createPost; restructured createPost

--- src/containers/CreatePost/index.jsx
+++ src/containers/CreatePost/index.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import RenderPostContents from './renderContents';
 
 const serverURL = process.env.REACT_APP_BE_URL || '';
 const BlogUploadAPI = process.env.REACT_APP_Create_Post || '';
@@ -9,6 +10,7 @@ const CreatePost = props => {
   const [title, setTitle] = useState('');
   const [contents, setContents] = useState([{ type: 'p' }]);
   const [addElement, setAddElement] = useState('');
+  const [generic, setGeneric] = useState(false);
   const history = useHistory();
 
   const handleContentsChange = (index, obj) => setContents([
@@ -43,49 +45,15 @@ const CreatePost = props => {
     if(canSubmit) {
       axios.post(serverURL + BlogUploadAPI,
         {
-          ...contents,
-          time: Date(),
-          author: props.user,
+          title: title,
+          content: contents,
+          insitute: props.insitute,
+          branch: props.branch,
+          tag: generic ? 'generic' : 'blog',
         }
       ).then(() => history.push(''));
     }
   }
-
-  const renderContents = () =>
-    contents.map((el, index) => {
-      if(el.type === 'p') {
-        return (
-          <div key={index}>
-            <button onClick={() => removeElement(index)}>Remove</button>
-            <input type='text' value={el.content}
-              onChange={e => handleContentsChange(index, { content: e.target.value })}
-              required
-            />
-          </div>
-        );
-      } else if(el.type === 'img') {
-        return (
-          <div key={index}>
-            <button onClick={() => removeElement(index)}>Remove</button>
-            <p><i>
-              [Please host the images somewhere online 
-              (like <a href='https://imgur.com/upload'>https://imgur.com/upload</a>)
-              and provide link here. The link will not be visible to readers once post is ready.]
-            </i></p>
-            <label htmlFor='src'>URL</label>
-            <input type='text' name='src' value={el.src}
-              onChange={e => handleContentsChange(index, { src: e.target.value })}
-            />
-            <img src={el.src} alt={el.description} />
-            <label htmlFor='description'>Image description</label>
-            <input type='text' name='description' value={el.description}
-              onChange={e => handleContentsChange(index, { description: e.target.value })}
-            />
-          </div>
-        );
-      }
-      return null;
-    });
 
   return (
     <div>
@@ -97,9 +65,18 @@ const CreatePost = props => {
           required
         />
       </div>
-      <div>{`{add main image optional}`}</div>
       <div>
-        {renderContents()}
+        <label htmlFor='generic'>Generic post</label>
+        <input type='radio' name='generic' checked={generic} onChange={() => setGeneric(true)} />
+        <label htmlFor='blog'>Blog post</label>
+        <input type='radio' name='blog' checked={!generic} onChange={() => setGeneric(false)} />
+      </div>
+      <div>
+        <RenderPostContents
+          contents={contents}
+          handleContentsChange={handleContentsChange}
+          removeElement={removeElement}
+        />
       </div>
       <select value={addElement} onChange={addNewElement}>
         <option value='' disabled selected>Add Element</option>
