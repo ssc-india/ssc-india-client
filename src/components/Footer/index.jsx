41d1b0b75f6308e93b18b1aa3f7237674a14acfb
Sentienta <sentientasins@gmail.com>
2021-10-22 17:28:16 +0530

    basic footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Footer = () =>
+  <div className='Footer'>
+    {`{Footer text}`}
+  </div>
+
+export default Footer;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,6 @@ import Navbar from './Navbar';
 import ListPosts from './ListPosts';
 import ListPostsFilter from './ListPostsFilter';
 import SidePanel from './SidePanel';
+import Footer from './Footer';
 
-export { Navbar, ListPostsFilter, ListPosts, SidePanel };
+export { Navbar, ListPostsFilter, ListPosts, SidePanel, Footer };
